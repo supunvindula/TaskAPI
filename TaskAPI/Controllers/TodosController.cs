4d3b12493ed124ff582d5ff6c6119de32a98dcd1
supunvindula <e16377@eng.pdn.ac.lk>
2022-01-30 13:02:16 +0530

    Created and Seperated files into models, services and controllers.

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+using TaskAPI.Services;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private TodoService _todoService;
+        public TodosController()
+        {
+            _todoService = new TodoService();
+        }
+        [HttpGet("{id?}")]
+        public IActionResult GetToDos(int? id)
+        {
+            var mytodos = _todoService.AllTodos();
+            if(id is null)
+            {
+                return Ok(mytodos);
+            }
+            mytodos = mytodos.Where(t => t.Id == id).ToList();
+            return Ok(mytodos);
+        }
+
+        
+    }
+}
